2cf4e99acc7b95e9ce198d2892552292641fff19
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-02-17 19:51:00 +0100

    Reprise du projet en 2023 après un an de break pour faire de la Data Science

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
-    'accounts'
+    'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links',
             ],
         },
     },
