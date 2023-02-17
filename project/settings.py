7072e049d858ba7dd2d6f00a2183860f82d40481
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-02-17 23:05:58 +0100

    Carts functionnality : add to cart using Session keys, Increment/decrement/remove Carts Item

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,9 @@ STATICFILES_DIRS = [
 # media files configurations
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
