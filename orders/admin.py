7e7898053c3c5d292dc7295e7b7fdb2bdd999c79
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-10 17:26:06 +0100

    implemented review and rating system

--- orders/admin.py
+++ orders/admin.py
@@ -5,11 +5,11 @@ from .models import Payment, Order, OrderProduct
 
 class OrderProductInline(admin.TabularInline):
     model = OrderProduct
-    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered')
+    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered', 'variations')
     extra = 0
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'is_ordered', 'created_at']
+    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'ip', 'is_ordered', 'created_at']
     list_filter = ['status', 'is_ordered']
     search_fields = ['order_number', 'first_name', 'last_name', 'phone', 'email']
     list_per_page = 20
