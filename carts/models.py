778b37f766d1c717b8d52e6a5111a938e4a1d05d
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-10 11:33:38 +0100

    cart app completed

--- carts/models.py
+++ carts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from store.models import Product, Variation
+from accounts.models import Account
 
 
 # Create your models here.
+
 class Cart(models.Model):
-    cart_id = models.CharField(max_length=250, blank=True, null=True)
+    cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -12,21 +14,15 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
     def sub_total(self):
         return self.product.price * self.quantity
 
-    # def __unicode__(self):
-    #     return self.product
-
-    # def __unicode__(self):
-    #     return f"{self.product}" or ''
-
-    def __str__(self):
-        return str(self.product) if self.product else ''
-
+    def __unicode__(self):
+        return self.product
